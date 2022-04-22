2d3e9abc39f7a1d2ddf445decdbc4873d1dfd6b4
Arthur Correnson <arthur.correnson@gmail.com>
2022-04-22 22:03:00 +0200

    Fix a bug in the solver

--- bin/test.ml
+++ bin/test.ml
@@ -2,20 +2,33 @@ open Puzzle
 open Spec
 open Search
 
-module MySpec : SPEC with type action = int * int = struct
+type increment = PLUS1 | PLUS2
+
+module MySpec : SPEC with type action = increment = struct
   type state = int
-  type action = int * int
-  let apply s _ = (s + 1) mod 10
-  let actions x = [(x, x + 1)]
+  
+  type action = increment
+
+  let apply s = function
+    | PLUS1 -> (s + 1) mod 10
+    | PLUS2 -> (s + 2) mod 10
+  
+  let actions _ = [PLUS1; PLUS2]
+  
   let goal s = s = 4
+
   let init = 0
-  let cost _ = 0
+  
+  let cost = function
+    | PLUS1 -> 2
+    | PLUS2 -> 1
 end
 
 module Problem = Solver(MySpec)
 
 let () =
   match Problem.solve () with
-  | None -> Printf.printf "No solution!\n"
-  | Some l ->
-    List.iter (fun (x, y) -> Printf.printf "%d -> %d\n" x y) l
+  | None    ->
+    Printf.printf "No solution!\n"
+  | Some l  ->
+    List.iter (function PLUS1 -> print_endline "+1" | PLUS2 -> print_endline "+2") l
--- src/search.ml
+++ src/search.ml
@@ -29,7 +29,7 @@ module Solver : SOLVER = functor (X: SPEC) -> struct
     (* Work list *)
     let queue = Heap.of_array [| 0, X.init |] in
     (* Table of predecessors *)
-    let preds = Hashtbl.create 10 in
+    let preds : (X.state, (X.state * X.action) option) Hashtbl.t = Hashtbl.create 10 in
     (* Marked states *)
     let marks = ref States.empty in
     (* Test if a state is marked *)
@@ -37,38 +37,37 @@ module Solver : SOLVER = functor (X: SPEC) -> struct
     (* Mark a state *)
     let mark x = marks := States.add x !marks in
     (* Build full path from [x] to [X.init] *)
-    let build_path (x, _) =
-      let rec step path (x, act) =
-        if x = X.init then
-          act::path
-        else
+    let build_path x =
+      let rec step path = function
+        | None -> path
+        | Some (x, act) ->
           step (act::path) (Hashtbl.find preds x)
       in
       step [] (Hashtbl.find preds x)
     in
     Hashtbl.add costs X.init 0;
+    Hashtbl.add preds X.init None;
     while not (Heap.is_empty queue) do
       let (xcost, x) = Heap.extract queue in
       if not (marked x) then begin
         List.iter (fun (act, y) ->
           let new_ycost = xcost + X.cost act in
           match Hashtbl.find_opt costs y with
-          | Some ycost when new_ycost > ycost ->
+          | Some ycost when new_ycost < ycost ->
             Hashtbl.add costs y new_ycost;
-            Hashtbl.add preds y (x, act);
+            Hashtbl.add preds y (Some (x, act));
             Heap.insert queue y new_ycost
           | None ->
             Hashtbl.add costs y new_ycost;
-            Hashtbl.add preds y (x, act);
+            Hashtbl.add preds y (Some (x, act));
             Heap.insert queue y new_ycost
           | _ -> ()
-
         ) (transitions x);
         mark x
       end
     done;
-    let reach = List.of_seq (Hashtbl.to_seq_values preds) in
-    let goal = List.find_opt (fun (x, _) -> X.goal x) reach in
+    let reach = List.of_seq (Hashtbl.to_seq_keys preds) in
+    let goal = List.find_opt X.goal reach in
     Option.map build_path goal
 end
 
