2f7ce08d07f6b9c7c38787a164bed6a8dfe54739
Arthur Correnson <arthur.correnson@gmail.com>
2022-05-11 18:03:52 +0200

    Implementation of minimax

--- src/games.ml
+++ src/games.ml
@@ -4,9 +4,32 @@ open Spec
     Solve the game for player [P1].
 *)
 module MinMax(X : GAME2) : sig
-  val solve : unit -> X.action list option
+  val play : X.state -> X.action option
 end = struct
-  let solve () = assert false
+
+  let arg_max' ((_, vx) as x) ((_, vy) as y) =
+    if vx > vy then x else y
+
+  let arg_max = function
+    | [] -> None
+    | x::xs -> Some (List.fold_left arg_max' x xs |> fst)
+
+  let rec expand_first s =
+    let next act = act, min_val (X.apply s act)
+    in List.map next (X.actions s)
+
+  and expand s =
+    let next act = min_val (X.apply s act)
+    in List.map next (X.actions s)
+  
+  and min_val s =
+    List.fold_left min (X.utility s) (expand s)
+
+  and max_val s =
+    List.fold_left max (X.utility s) (expand s)
+
+  let play s =
+    arg_max (expand_first s)
 end
 
 (** Minimax with Alpha Beta pruning.
