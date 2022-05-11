9a85481e385dcdc62bd384372cc5043b1439b74c
Arthur Correnson <arthur.correnson@gmail.com>
2022-05-11 18:21:38 +0200

    add memoization to minmax algorithm

--- src/games.ml
+++ src/games.ml
@@ -1,5 +1,15 @@
 open Spec
 
+let cache f =
+  let h = Hashtbl.create 10 in
+  fun x ->
+    match Hashtbl.find_opt h x with
+    | Some fx -> fx
+    | None ->
+      let fx = f x in
+      Hashtbl.add h x fx;
+      fx
+
 (** Minimax for 2-players games.
     Solve the game for player [P1].
 *)
@@ -18,18 +28,20 @@ end = struct
     let next act = act, min_val (X.apply s act)
     in List.map next (X.actions s)
 
-  and expand s =
-    let next act = min_val (X.apply s act)
+  and expand s f =
+    let next act = f (X.apply s act)
     in List.map next (X.actions s)
   
   and min_val s =
-    List.fold_left min (X.utility s) (expand s)
+    List.fold_left min (X.utility s) (expand s max_val)
 
   and max_val s =
-    List.fold_left max (X.utility s) (expand s)
+    List.fold_left max (X.utility s) (expand s min_val)
 
-  let play s =
+  let minmax s =
     arg_max (expand_first s)
+  
+  let play = cache minmax
 end
 
 (** Minimax with Alpha Beta pruning.
